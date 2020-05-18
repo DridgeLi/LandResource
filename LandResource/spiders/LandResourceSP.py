53115e9c6af8db41bd86242aaeb7b79b74a0ac3e
DridgeLi <L506058080@outlook.com>
2020-05-18 22:26:59 +0800

    2020518：采用scrapy-splash方法模拟鼠标点击/未安装docker，未开启splash服务 20200519：建议公司进行另一方法：逐一爬取标题，直到找到第一个“成公资土拍告”，以此匹配

--- LandResource/spiders/LandResourceSP.py
+++ LandResource/spiders/LandResourceSP.py
@@ -1,11 +1,34 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import scrapy_splash
+
+
+# 模拟点击采用js的方式
+script = """
+function main(splash, args)
+  splash.images_enabled = false
+  assert(splash:go(args.url))
+  assert(splash:wait(0.5))
+  js = string.format("document.querySelector('div[@<div class="option unchoosed xh-highlight" data-value="7">
+                                            拍卖公告
+                                        </div>').click();", args.page)
+  splash:runjs(js)
+  assert(splash:wait(1))
+  return splash:html()
+end
+"""
 
 
 class LandresourcespSpider(scrapy.Spider):
     name = 'LandResourceSP'
-    allowed_domains = ['https://www.cdggzy.com/site/LandTrade/LandList.aspx']
+    allowed_domains = ['www.cdggzy.com']
     start_urls = ['https://www.cdggzy.com/site/LandTrade/LandList.aspx/']
 
+    def start_requests(self):
+        for url in self.start_urls:
+            yield scrapy_splash.SplashRequest(url, callback=self.parse, endpoint='execute', args={
+                'lua_source':script
+            })
+
     def parse(self, response):
-        pass
+        print(response.xpath(".//div[@class='col-xs-10 infotitle']/a[1]").extract())
