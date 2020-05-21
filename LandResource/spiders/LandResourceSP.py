efac80cbdece42a93a1e63204b769cf40bd34d45
DridgeLi <L506058080@outlook.com>
2020-05-21 19:40:22 +0800

    Spider of LandResource-CD

--- LandResource/spiders/LandResourceSP.py
+++ LandResource/spiders/LandResourceSP.py
@@ -40,9 +40,10 @@ class LandresourcespSpider(scrapy.Spider):
 class LandresourcespSpider(scrapy.Spider):
     name = 'LandResourceSP'
     allowed_domains = ['www.cdggzy.com']
-    start_urls = ['https://www.cdggzy.com/site/LandTrade/LandList.aspx/']
+    start_urls = ['https://www.cdggzy.com/site/LandTrade/LandList.aspx?id=6719']
 
     print("开始爬取网页...")
+
 
     def parse(self, response):
         print("开始爬取第一页")
@@ -51,10 +52,12 @@ class LandresourcespSpider(scrapy.Spider):
         notice_list = response.xpath("//div[@id='contentlist']/div[@class='row contentitem']")
         for i_notice in notice_list:
             landresource_item['notice_name'] = i_notice.xpath(".//div[@class='col-xs-10 infotitle']/a/text()").extract_first()
+            print(landresource_item['notice_name'])
             landresource_item['notice_link'] = i_notice.xpath(".//div[@class='col-xs-10 infotitle']/a[@href]").extract()
             while re.search(r"成公资土拍告", landresource_item['notice_name']):
-                notice_number = int(landresource_item['notice_time'][31:32])
+                notice_number = int(landresource_item['notice_name'][31:32])
                 if notice_number > latest_notice_number:
+                    print("找到公告号")
                     yield scrapy.Request(landresource_item['notice_link'], callback=self.parse_currentNotice(), meta={landresource_item})
                 else:
                     break
@@ -64,6 +67,7 @@ class LandresourcespSpider(scrapy.Spider):
         notice_content = response.xpath("//div[@id='noticecontent']")
         for i_notice_content in notice_content:
             landresource_item['time_list'] = i_notice_content.xpath(".//div[@class='noticepubtime']/text()").extract_first()
-
+            landresource_item['serial_number'] = i_notice_content.xpath(".//iframe/table/tbody/tr[3]/span/text()").extract_first()
+            print(landresource_item)
 
 
